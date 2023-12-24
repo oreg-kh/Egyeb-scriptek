221bfc53c6c5e6a889f74dd3aa000669e7742080
oreg-kh <60749041+oreg-kh@users.noreply.github.com>
2023-12-24 11:11:56 +0100

    Update Új küldetés.js

--- Új küldetés.js
+++ Új küldetés.js
@@ -5,14 +5,17 @@ javascript:Questlines.showDialog(0, 'main-tab');void(0)
 javascript:Questlines.showDialog(101, 'main-tab');void(0)
 
 //Jutalmak:
-javascript:Dialog.fetch("quest", "new_quests", {
-                ajax: "quest_popup",
-                tab: "main-tab",
-                quest: "0"
-            }, function(s) {
-                $(".quest-popup-body .tab:eq(0)").removeClass("active-tab");
-                $(".quest-popup-body .tab:eq(2)").addClass("active-tab");
-});void(0)
+javascript:
+Dialog.fetch("quest", "new_quests", {
+    ajax: "quest_popup",
+    tab: "main-tab",
+    quest: "0"
+}, function() {
+    $(".quest-popup-body .tab:eq(0)").removeClass("active-tab");    
+    $("#reward-tab").addClass("active-tab");
+    Questlines.init()
+});
+void(0);
 
 //Mentorküldetések:
 javascript:Questlines.showDialog(100, 'main-tab');void(0)
