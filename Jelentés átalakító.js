ffd10f24e83c40c9c1acb5de8683e180aefda782
oreg-kh <60749041+oreg-kh@users.noreply.github.com>
2023-12-28 11:38:50 +0100

    Update Jelentés átalakító.js

--- Jelentés átalakító.js
+++ Jelentés átalakító.js
@@ -1,6 +1,6 @@
 javascript:
-      var szoveg = $('#message').text().replace(/- \[report\]/g,"[Spoiler][Report_display]").replace(/\[\/report\]/g,"[/Report_display][/Spoiler]").replace("Továbbított jelentések:", "").trim();
-      $('#message').text(szoveg).select();
-      document.execCommand("Copy");
-      UI.InfoMessage("A jelentések a vágólapra másolva! <br> Author: öreg",3000);
+var text = $('#message').text().replace(/- \[report\]/g,"[Spoiler][Report_display]").replace(/\[\/report\]/g,"[/Report_display][/Spoiler]").replace(/^.*\n/, "").trim();
+$('#message').text(text).select();
+document.execCommand("Copy");
+UI.InfoMessage("A jelentések a vágólapra másolva!",3000);
 void(0);
