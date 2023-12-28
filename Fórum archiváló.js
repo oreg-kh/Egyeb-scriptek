06a57c24b84e1713a832f046274eaab6498c8d33
oreg-kh <60749041+oreg-kh@users.noreply.github.com>
2023-12-28 11:43:55 +0100

    Update Fórum archiváló.js

--- Fórum archiváló.js
+++ Fórum archiváló.js
@@ -1,19 +1,35 @@
 javascript:
-      var newline = "\n\n",
-          topic_name = $(".forum-content.clearfix").find("h2").text(),
-          server = game_data.world,
-          page_number = $(".clearfix").find("strong").last().text(),
-          page_numbers = (page_number == "") ? "1" : page_number.slice(2, -2),
-          textarea = '<textarea id="scriptused_text" cols="90" rows="15"></textarea>',
-          comments = "";
-      $('.spoiler').find('span').css('display', 'all').show();
-      $(".postheader_left").each(function (key, val) {
-            var name = $(val)[0].innerText;
-            var text = $(".text")[key].innerText;
-            var comment = '[table][**]' + name + '[/**]' + newline + '[*]' + text + '[/table]';
-            comments += comment;
-      });
-      $('h2:eq(0)').after(textarea);
-      $('textarea#scriptused_text').val('[size=12][b]' + server + ": " + topic_name + '[/b][/size]' + newline + comments + newline + '[i]' + page_numbers + ". oldal[/i]").select();
-      $("#scriptused_text").width($("#forum_box").eq(0).width());
+var finaltext = "";
+var post = 0;
+var textarea = '<textarea id="scriptused_text" cols="90" rows="15"></textarea>';
+var url = "forum&screenmode=view_thread&thread_id=1&answer=true&quote_id=";
+
+function getPlayersInformation(id) {
+	return $.ajax({url: `${game_data.link_base_pure + url + id}`, type: 'GET', async: false});
+}
+
+async function comment() {
+    var postheaderleft = $(".postheader_left").eq(post).find("a").text().trim();
+    var tribe = postheaderleft.split(" - ")[0];
+    var name = postheaderleft.split(" - ")[1];
+    var date = $(".igmline-date").eq(post).text().trim();
+    var id = $(".post").eq(post++).find("a").attr("name");
+    var data = await getPlayersInformation(id);
+    var message = $(data).find("#message").text().trim();
+    var text = message.replace(/(\[.*?\])/, "").replace(/\[[^\][]*]$/, "");
+    finaltext += `[table][**][ally]${tribe}[/ally] - [player]${name}[/player]  ${date}[/**][/table]${text }`;
+
+    setTimeout(() => {
+        if(post < $(".post").length) {
+            comment();
+        } else {
+            $("#forum_box").before(textarea);
+            $("textarea#scriptused_text").val(finaltext).select();
+            $("#scriptused_text").width($("#forum_box").eq(0).width());
+            console.log(finaltext);
+        }
+    }, 200);
+}
+
+comment()
 void(0);
