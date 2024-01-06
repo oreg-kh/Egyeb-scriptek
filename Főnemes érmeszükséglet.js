4939d3fb2bf783d74737f360bed46ab79d5c50d2
oreg-kh <60749041+oreg-kh@users.noreply.github.com>
2024-01-06 07:14:45 +0100

    Update Főnemes érmeszükséglet.js

--- Főnemes érmeszükséglet.js
+++ Főnemes érmeszükséglet.js
@@ -1,20 +1,21 @@
 javascript:
-function numberWithCommas(x) {
-        var parts = x.toString().split(".");
-        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-        return parts.join(".");
-}
-var fonemes_korlat = prompt("Adj meg egy tetszőleges főnemes-korlátot és eredményül megkapod a hozzá szükséges aranyérmék számát.");
-var number = Number(fonemes_korlat);
-try {
-        if (isNaN(number) == true || fonemes_korlat == "" || number < 0 || Number.isInteger(BigInt(number))) {
-                UI.ErrorMessage("Csak pozitív egész szám adható meg.", 5000);
-        } else if (!fonemes_korlat) {
-        } else {
-                UI.SuccessMessage(numberWithCommas(number) + " főnemeshez " + numberWithCommas([number * (number + 1)] / 2) + " érmét szükséges veretni.", 10000);
-        }
-} catch (error) {
-        UI.ErrorMessage("Csak pozitív egész szám adható meg.", 5000);
-        console.log(error);
+(function () {
+    let buttonCancel = {
+        text: "Bezár",
+        callback: () => {},
+        cancel: true
+    };
+    let message = `
+        <p style="font-size:14px">Adj meg egy tetszőleges főnemes-korlátot és eredményül megkapod a hozzá szükséges aranyérmék számát.</p>
+        <input id="noble_value" maxlength="4" oninput=calculate(this.value) style="width: 40px;" type="number" value="">`
+        window.UI.ConfirmationBox(message, [buttonCancel], "coin_calculator", true, true);
+})()
+function calculate(val) {
+    if (val != "") {
+        $("#cointext").remove();
+        var noble = parseInt(val);
+        var coin = [noble * (noble + 1)] / 2;
+        $("input#noble_value").after(`<p id="cointext" style="font-size:14px"><b>${noble} főnemeshez ${coin} érmét szükséges veretni.</b></p>`);
+    }
 }
 void(0);
