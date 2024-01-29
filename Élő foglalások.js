6bf5ad53c13f9d9f889cd7e59f24815da1493866
oreg-kh <60749041+oreg-kh@users.noreply.github.com>
2024-01-29 16:20:23 +0100

    Update Élő foglalások.js

--- Élő foglalások.js
+++ Élő foglalások.js
@@ -1,31 +1,41 @@
 javascript:
-var text = {
-    url1:"https://api.codetabs.com/v1/proxy?quest=hu.twstats.com/",
-    url2:"/index.php?page=ennoblements&live=live"
-};
-var url = text.url1 + game_data.world + text.url2;
-$("<div>").load(url+" #main", function(responseTxt,statusTxt,xhr){
-    if ($(responseTxt).find("tr").hasClass("r1")) {
-        table = $(responseTxt).find(".widget").first();
-    } else {
-        table = "Ezen a szerveren most nincs élő foglalás.";
-    }
-    Dialog.show("foglalasok", table, function() {UI.SuccessMessage("Jó játékot :)")}),
-    $("#popup_box_foglalasok").css("width","fit-content");
-    $("#popup_box_foglalasok").find(".widget").find("a").each(function(key, val) {
-        var value = $(val).attr("href");
-        /*village*/
-        if ($(val).is('[href*="index.php?page=village&id="]')) {
-            $(val).attr('href', value.replace("index.php?page=village&id=", "/game.php?village=" + game_data.village.id + "&screen=info_village&id="));
+var api = "https://api.codetabs.com/v1/proxy?quest=hu.twstats.com/";
+var twstats = "/index.php?page=ennoblements&live=live";
+var url = api + game_data.world + twstats;
+var indexvillage = "index.php?page=village&id=";
+var indexplayer = "index.php?page=player&id=";
+var indextribe = "index.php?page=tribe&id=";
+var screenvillage = "&screen=info_village&id=";
+var screenplayer = "&screen=info_player&id=";
+var screenally = "&screen=info_ally&id=";
+var game = "/game.php?village=";
+
+    $.get(url, function(data) {
+        if ($(data).find("tr").hasClass("r1")) {
+            table = $(data).find(".widget").eq(1).find("tr");
+        } else {
+            table = "Ezen a szerveren most nincs élő foglalás.";
         }
-        /*player*/
-        if ($(val).is('[href*="index.php?page=player&id="]')) {
-            $(val).attr('href', value.replace("index.php?page=player&id=", "/game.php?village=" + game_data.village.id + "&screen=info_player&id="));
-        }
-        /*tribe*/
-        if ($(val).is('[href*="index.php?page=tribe&id="]')) {
-            $(val).attr('href', value.replace("index.php?page=tribe&id=", "/game.php?village=" + game_data.village.id + "&screen=info_ally&id="));
-        }
-    });
-});
+
+        Dialog.show("foglalasok", function(container) {  
+            $(`<table class="vis" id="table"><tbody></tbody></table>`).appendTo(container);
+            $(table).appendTo("#table > tbody");
+        })
+        
+        $("#table").find("a").each(function(key,val) {
+            var value = $(val).attr("href");
+            /*village*/
+            if ($(val).is(`[href*="${indexvillage}"]`)) {
+                $(val).attr("href", value.replace(indexvillage, game + game_data.village.id + screenvillage));
+            }
+            /*player*/
+            if ($(val).is(`[href*="${indexplayer}"]`)) {
+                $(val).attr("href", value.replace(indexplayer, game + game_data.village.id + screenplayer));
+            }
+            /*tribe*/
+            if ($(val).is(`[href*="${indextribe}"]`)) {
+                $(val).attr("href", value.replace(indextribe, game + game_data.village.id + screenally));
+            }
+        })
+    })
 void(0);
